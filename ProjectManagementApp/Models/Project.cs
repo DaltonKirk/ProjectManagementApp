6f9f58b77bc5b0498330631d4f36e59bc19918c5
DaltonKirk <daltonkirk5@gmail.com>
2018-09-24 19:04:01 +0100

    Added Project model and scaffolded CRUD pages

--- ProjectManagementApp/Models/Project.cs
+++ ProjectManagementApp/Models/Project.cs
@@ -1,10 +1,15 @@
 using System.ComponentModel.DataAnnotations;
 namespace ProjectManagementApp.Models
 {
+    public enum ProjectStage { PreProduction, PreAlpha, Alpha, Beta, Master, Launch, LiveOps };
+
     public class Project {
         [Key]
         public int Id {get;set;}
 
         public string Name {get;set;}
+        public string Description { get; set; }
+
+        public ProjectStage Stage { get; set; }
     }
 }
